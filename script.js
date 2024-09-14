87438e9b2c31ba1e8249ffb469056676bf9187c8
Nikesh Borkar <99666800+NikeshBorkar@users.noreply.github.com>
2024-09-15 00:05:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 // complete the given function
 
 function palindrome(str){
+	let end=str.length-1;
+	let start=0;
+	while (start<end) {
+		if(str[end]==str[start]){
+			end--;
+			start++;
+		}else{
+			return false
+		}
+		
+	}
+	return true;
 
 }
 module.exports = palindrome
